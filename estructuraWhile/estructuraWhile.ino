b3e279db0c59dc67967c7a7ba3d8f5de84fe8933
AlbertoPimentel <albertopimentel04@gmail.com>
2021-11-05 14:32:43 -0600

    Estructura while / Do while

---
+++ estructuraWhile/estructuraWhile.ino
@@ -0,0 +1,49 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  /*
+    //Sintaxis
+    //Mientras
+    while (condicion ) {
+    //Instrucciones
+    }
+  */
+
+  /*
+    for(int i = 1; i <= 10; i++){
+    Serial.println(i);
+    delay(1000);
+    }
+  */
+
+Serial.println("Incremento While");
+  int i = 1; // Variable con valor 1
+  while ( i <= 10 ) {
+    Serial.println(i);
+    delay(500);
+    i++; //!Importante no olvidar incrementar o decrementar
+  }
+
+
+Serial.println("Decremento While");
+  int j = 10;
+  while (j >= 1) {
+    Serial.println(j);
+    delay(1000);
+    j--;
+  }
+
+
+  //Do - While
+  Serial.println("Incremento Do While");
+  int k = 1;
+  do {
+    Serial.println(k);
+    delay(1000);
+    k++;
+  } while (k <= 10);
+
+
+}
