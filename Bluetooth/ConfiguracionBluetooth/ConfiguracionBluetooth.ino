c222c687a2a378fe62022206fc64c52af2e351c4
Alberto <albertopimentel04@gmail.com>
2022-03-03 16:44:29 -0600

    test

--- Bluetooth/ConfiguracionBluetooth/ConfiguracionBluetooth.ino
+++ Bluetooth/ConfiguracionBluetooth/ConfiguracionBluetooth.ino
@@ -3,19 +3,35 @@
 //A8 = 62 ->TX
 //A9 = 63 ->RX
 SoftwareSerial miBT(63, 62); // (RX, TX)   RX=63, TX=62
+int led = 5;
 void setup() {
+  pinMode(led, OUTPUT);
   Serial.begin(9600); //Abrimos la comunicacion Serial
   Serial.println("Listo");
   //INICIALIZAR comunicacion serie del modulo Bluetooth
   miBT.begin(9600);
 }
-
+int lectura;
 void loop() {
   //Bluetooth a la computadora
   if (miBT.available()) {
+    lectura = miBT.read();
     Serial.write(miBT.read()); //Si hay datos los muestro en el monitor serial (read())
     //Serial.write escribo en el monitor
+
+    if (lectura == '1') {
+      Serial.println("led on");
+      digitalWrite(led, HIGH);
+    }
+    if (lectura == '0') {
+      Serial.println("led off");
+      digitalWrite(led, LOW);
+    }
+
   }
+
+
+
 
   //Proceso inverso
   //Mandar informacion de la computadora al Bluetooth
---
+++ Test/joystickTest/joystickTest.ino
@@ -0,0 +1,82 @@
+//Motores
+int izquierdoDelante = 40;
+int izquierdoAtras = 41;
+int derechoDelante = 39;
+int derechoAtras = 38;
+
+//Joystick
+int x;
+int y;
+void setup() {
+  pinMode(izquierdoAtras, OUTPUT);
+  pinMode(izquierdoDelante, OUTPUT);
+  pinMode(derechoAtras, OUTPUT);
+  pinMode(derechoDelante, OUTPUT);
+
+  Serial.begin(9600);
+  //Entradas analogicas no requieren inicializacion.
+}
+
+void loop() {
+  x = analogRead(A0);
+  y = analogRead(A1);
+
+  Serial.println("");
+  Serial.print("x: ");
+  Serial.print(x);
+  Serial.println("");
+  Serial.print("y: ");
+  Serial.print(y);
+
+
+
+  //Detener motores
+  if (y > 480 && y < 520 && x > 480 && x < 520) {
+    digitalWrite(izquierdoDelante, LOW);
+    digitalWrite(derechoDelante, LOW);
+    digitalWrite(izquierdoAtras, LOW);
+    digitalWrite(derechoAtras, LOW);
+  }
+
+  //Avanzar Delante
+  if (y > 520 && y <= 1023) {
+    digitalWrite(izquierdoDelante, HIGH);
+    digitalWrite(derechoDelante, HIGH);
+    Serial.println("DELANTE");
+  } else {
+    digitalWrite(izquierdoDelante, LOW);
+    digitalWrite(derechoDelante, LOW);
+  }
+
+  //Avanzar Atras
+  if (y >= 0 && y < 480) {
+    digitalWrite(izquierdoAtras, HIGH);
+    digitalWrite(derechoAtras, HIGH);
+    Serial.println("ATRAS");
+  } else {
+    digitalWrite(izquierdoAtras, LOW);
+    digitalWrite(derechoAtras, LOW);
+  }
+
+  //Izquierda
+  if (x >= 0 && x < 480 ) {
+    digitalWrite(derechoDelante, HIGH);
+    
+    Serial.println("IZQUIERDA");
+  } else {
+     digitalWrite(izquierdoDelante, LOW);
+  }
+
+
+  //Derecha
+  if (x > 520  && x <= 1023) {
+    digitalWrite(izquierdoDelante, HIGH);
+    Serial.println("DERECHA");
+  } else {
+    digitalWrite(derechoDelante, LOW);
+  }
+
+
+
+
+}
